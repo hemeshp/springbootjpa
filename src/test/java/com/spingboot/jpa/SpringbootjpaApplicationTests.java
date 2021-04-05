65188cc62b987f3d7463df9d7e0a9ff3d753c554
HEMESH-LAPTOP\Lab <2hemesh@gmail.com>
2021-04-05 20:44:42 +0530

    Removed test related error

--- src/test/java/com/spingboot/jpa/SpringbootjpaApplicationTests.java
+++ src/test/java/com/spingboot/jpa/SpringbootjpaApplicationTests.java
@@ -1,9 +1,10 @@
 package com.spingboot.jpa;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
 class SpringbootjpaApplicationTests {
 
 	@Test
